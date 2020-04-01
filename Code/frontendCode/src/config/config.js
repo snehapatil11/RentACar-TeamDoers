e9d67992918ec0525acec2bb4dc17482c1586db9
ambika2494 <61039307+ambika2494@users.noreply.github.com>
2020-03-31 23:51:12 -0700

    adding configuration for backend URL and price

--- Code/frontendCode/src/config/config.js
+++ Code/frontendCode/src/config/config.js
@@ -1,1 +1,11 @@
 //configuration
+ var valuesExport = {
+    url:'http://localhost:3001/',
+    price:{
+        "1":300,
+        "2":500,
+        "3":750,
+    }
+}
+
+export default valuesExport;
