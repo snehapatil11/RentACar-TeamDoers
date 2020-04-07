fbe6200dc52d310149125f43f6b8db20f9bb60c3
sneha <sneha.magdum11@gmail.com>
2020-04-07 12:09:27 -0700

    Adding build folder for deployment

---
+++ Code/frontendCode/build/precache-manifest.354ba3996806b3adba1f73830d590cb4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51ba3ce7beb9f17478f86563458ff0b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38308624b00d6ba993c9",
+    "url": "/static/css/2.ccbb2cf0.chunk.css"
+  },
+  {
+    "revision": "b6e395dd5025ca0b837a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "38308624b00d6ba993c9",
+    "url": "/static/js/2.9629a276.chunk.js"
+  },
+  {
+    "revision": "0836c1fd6bd966d0cecc7d3fba8b7798",
+    "url": "/static/js/2.9629a276.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6e395dd5025ca0b837a",
+    "url": "/static/js/main.152593b6.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  },
+  {
+    "revision": "1ca21d94549427580f9418e9113e61cc",
+    "url": "/static/media/rentcar.1ca21d94.jpg"
+  }
+]);
