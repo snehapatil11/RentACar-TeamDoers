5734f86e3b318d40a1219b3dc525ad4e7d1ba5a6
ambika2494 <61039307+ambika2494@users.noreply.github.com>
2020-04-18 10:49:43 -0700

    successful login, storing user details

--- Code/frontendCode/src/components/login.js
+++ Code/frontendCode/src/components/login.js
@@ -3,6 +3,11 @@ import { Form, Input, Button, Alert, Select } from 'antd';
 import "antd/dist/antd.css";
 import Headers from './header';
 import Footer from './footer';
+import axios from 'axios';
+import valuesExport from '../config/config';
+import { Redirect } from 'react-router';
+import UserTypeFactory from './userTypeFactory';
+
 
 const { Option } = Select;
 
@@ -23,7 +28,47 @@ class Login extends Component {
 
     handleSubmit = values => {
         console.log(values)
-    };
+
+        axios.post(valuesExport.url + 'user/login/', JSON.stringify(values), {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+
+            }
+        })
+            .then(async res => {
+                if (res.status >= 400) {
+                    console.log(res)
+
+                    this.setState({
+                        showError: true,
+                    })
+
+                }
+                else {
+
+                    await localStorage.setItem("user_id", res.data[0].userId);
+                    await localStorage.setItem("userType", values.userType);
+                    await localStorage.setItem("email_id", values.emailid);
+                    await localStorage.setItem("name", res.data[0].name);
+
+                    let userTypeobj;
+
+
+                    userTypeobj = UserTypeFactory.userTypeURL(res.userType);
+                    this.setState({
+                        redirectPage: <Redirect to={{ pathname: userTypeobj }} />
+                    })
+                }
+            })
+            .catch(err => {
+                console.log(err)
+                this.setState({
+                    showError: true,
+                })
+
+            })
+    }
 
     failedSubmit = (error) => {
         console.log(error)
@@ -65,12 +110,12 @@ class Login extends Component {
                         style={{ paddingTop: '2%' }}
                     >
                         <Form.Item
-                            label="Username"
-                            name="username"
+                            label="Email ID"
+                            name="emailid"
                             rules={[
                                 {
                                     required: true,
-                                    message: 'Please input your username!',
+                                    message: 'Please input your emailID!',
                                 },
                             ]}
                         >
